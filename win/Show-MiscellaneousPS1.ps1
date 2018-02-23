882d81200442f0356a5f7af186124988cbcb4004
valdeza <valdeza@users.noreply.github.com>
2018-02-23 02:15:42 -0500

    win: Reorganised: Moved PowerShell commands to dedicated file
    from windows-common-actions.md

---
+++ win/Show-MiscellaneousPS1.ps1
@@ -0,0 +1,17 @@
+﻿# Miscellaneous commands stuck into this .ps1 for delicious syntax highlighting.
+
+# When commands are added to this, most would probably not want to execute these arbitrary one-liners.
+{ # Wrapped in ScriptBlock to prevent script execution
+    # 'throw' statement to *really* prevent script execution
+    throw 'Script execution prohibited.'
+
+
+
+    # Linux `tail -f` equivalent
+    Get-Content $varFilepath -Wait
+
+
+
+    # List available methods for command outputs
+    $varCommand | Get-Member
+}
